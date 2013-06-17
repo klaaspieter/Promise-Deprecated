7c04d6378d25e1d3b69ce6359592df2473fce065
Klaas Pieter Annema <klaaspieter@annema.me>
2013-06-17 11:14:46 +0200

    Implement 3.2.4 specs

---
+++ PromiseTests/PRMPromise3.2.4Spec.m
@@ -0,0 +1,46 @@
+//
+//  PRMPromise3.2.4Spec.m
+//  Promise
+//
+//  Created by Klaas Pieter Annema on 17-06-13.
+//  Copyright 2013 __MyCompanyName__. All rights reserved.
+//
+
+#import "Kiwi.h"
+
+
+SPEC_BEGIN(PRMPromise3_2_4Spec)
+
+__block NSDictionary *dummy = @{@"dummy": @"dummy"};
+
+describe(@"3.2.4: `then` must return before `onFulfilled or `onRejected` is called", ^{
+    it(@"fulfilled", ^{
+        __block BOOL thenHasReturned = NO;
+        __block BOOL blockWasCalled = NO;
+        
+        PRMPromise *promise = fulfilled(dummy);
+        promise.then(^(id theValue) {
+            [[theValue(thenHasReturned) should] beYes];
+            blockWasCalled = YES;
+        }, nil);
+        
+        thenHasReturned = YES;
+        [[theValue(blockWasCalled) should] beYes];
+    });
+    
+    it(@"rejected", ^{
+        __block BOOL thenHasReturned = NO;
+        __block BOOL blockWasCalled = NO;
+        
+        PRMPromise *promise = rejected(dummy);
+        promise.then(nil, ^(id theReason) {
+            [[theValue(thenHasReturned) should] beYes];
+            blockWasCalled = YES;
+        });
+        
+        thenHasReturned = YES;
+        [[theValue(blockWasCalled) should] beYes];
+    });
+});
+
+SPEC_END
